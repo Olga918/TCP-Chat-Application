95b097917e3c28d869df5dda3c9179fc892c858c
Olga918 <olgafiliepova@gmail.com>
2025-10-20 20:01:16 +0200

    Add files via upload
    add

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listLog = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chkComputer = new System.Windows.Forms.CheckBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(39, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(386, 217);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки сервера";
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(112, 158);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(142, 38);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Остановить сервер";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnStart.Location = new System.Drawing.Point(112, 86);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(142, 36);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Запустить сервер";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(112, 48);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPort.Multiline = true;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(146, 30);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "49200";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(139, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Порт";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox2.Controls.Add(this.listLog);
+            this.groupBox2.Location = new System.Drawing.Point(504, 118);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(500, 311);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Лолг сервера";
+            // 
+            // listLog
+            // 
+            this.listLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listLog.FormattingEnabled = true;
+            this.listLog.ItemHeight = 16;
+            this.listLog.Location = new System.Drawing.Point(22, 33);
+            this.listLog.Margin = new System.Windows.Forms.Padding(4);
+            this.listLog.Name = "listLog";
+            this.listLog.Size = new System.Drawing.Size(451, 226);
+            this.listLog.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox3.Controls.Add(this.chkComputer);
+            this.groupBox3.Controls.Add(this.btnSend);
+            this.groupBox3.Controls.Add(this.txtMessage);
+            this.groupBox3.Location = new System.Drawing.Point(39, 281);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(386, 188);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Отправка сообщений";
+            // 
+            // chkComputer
+            // 
+            this.chkComputer.AutoSize = true;
+            this.chkComputer.Location = new System.Drawing.Point(64, 139);
+            this.chkComputer.Margin = new System.Windows.Forms.Padding(4);
+            this.chkComputer.Name = "chkComputer";
+            this.chkComputer.Size = new System.Drawing.Size(170, 20);
+            this.chkComputer.TabIndex = 2;
+            this.chkComputer.Text = "Режим компьютенра";
+            this.chkComputer.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(64, 87);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(210, 44);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(64, 40);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(234, 24);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(1066, 554);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ChatServer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listLog;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.CheckBox chkComputer;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatServer
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener _server;
+        private Socket _clientSocket;
+        private Random _random = new Random();
+        private string[] _autoResponses = {
+            "Привет! Как дела?",
+            "Интересно... Расскажи больше!",
+            "Я программа, но стараюсь помочь!",
+            "Что думаешь об этом?",
+            "Сегодня хороший день!",
+            "Продолжай, я слушаю...",
+            "Это интересная тема!",
+            "Хочешь поговорить о чем-то?",
+            "Как прошел твой день?",
+            "До свидания! Было приятно пообщаться!"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int port = int.Parse(txtPort.Text);
+                _server = new TcpListener(IPAddress.Any, port);
+                _server.Start();
+
+                AddLog($"Сервер запущен на порту {port}");
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+
+                // Ждем подключения клиента
+                _ = Task.Run(async () => await AcceptClient());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+        private async Task AcceptClient()
+        {
+            try
+            {
+                _clientSocket = await _server.AcceptSocketAsync();
+
+                AddLog("Клиент подключен!");
+                btnSend.Enabled = true;
+
+                // Запускаем прием сообщений
+                _ = Task.Run(async () => await ReceiveMessages());
+            }
+            catch (Exception ex)
+            {
+                AddLog($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private async Task ReceiveMessages()
+        {
+            var buffer = new byte[1024];
+
+            while (_clientSocket != null && _clientSocket.Connected)
+            {
+                try
+                {
+                    int bytesRead = await _clientSocket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+                    if (bytesRead == 0) break;
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    AddLog($"Клиент: {message}");
+
+                    // Проверка на "Bye"
+                    if (message.Equals("Bye", StringComparison.OrdinalIgnoreCase))
+                    {
+                        AddLog("Клиент попрощался");
+                        _clientSocket.Close();
+                        break;
+                    }
+
+                    // Автоответ в режиме компьютера
+                    if (chkComputer.Checked)
+                    {
+                        string response = GetAutoResponse();
+                        byte[] responseData = Encoding.UTF8.GetBytes(response);
+                        await _clientSocket.SendAsync(new ArraySegment<byte>(responseData), SocketFlags.None);
+                        AddLog($"Я (авто): {response}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AddLog($"Ошибка: {ex.Message}");
+                    break;
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _server?.Stop();
+                _clientSocket?.Close();
+
+                AddLog("Сервер остановлен");
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                btnSend.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                AddLog($"Ошибка: {ex.Message}");
+            }
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text.Trim();
+            if (!string.IsNullOrEmpty(message) && _clientSocket != null)
+            {
+                try
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    _clientSocket.SendAsync(new ArraySegment<byte>(data), SocketFlags.None);
+                    AddLog($"Я: {message}");
+                    txtMessage.Clear();
+
+                    if (message.Equals("Bye", StringComparison.OrdinalIgnoreCase))
+                    {
+                        _clientSocket.Close();
+                        btnSend.Enabled = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AddLog($"Ошибка отправки: {ex.Message}");
+                }
+            }
+        }
+
+        private string GetAutoResponse()
+        {
+            return _autoResponses[_random.Next(_autoResponses.Length)];
+        }
+
+        private void AddLog(string message)
+        {
+            if (listLog.InvokeRequired)
+            {
+                Invoke(new Action<string>(AddLog), message);
+                return;
+            }
+            listLog.Items.Add($"{DateTime.Now:HH:mm:ss} - {message}");
+            listLog.TopIndex = listLog.Items.Count - 1;
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
